b4bab6589c55dfc12c9e23a4f561ca9bbfcf6856
M-Nad <106812302+M-Nad@users.noreply.github.com>
2024-10-22 16:33:54 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ async function handler(_req: Request): Promise<Response> {
   headers.append("Content-Type", "application/json");
 
   const similarityRequestBody = JSON.stringify({
-    word1: lastPart,
+    word1: "poirier",
     word2: "pommier",
   });
 
